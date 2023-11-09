8ed919b6a98e973da634eebd5c30014fc744351c
SunshineRoque <sunshineararoque@gmail.com>
2023-11-09 17:45:51 +0800

    Feature 6 - Add the migration for address model

--- db/migrate/20231109093007_create_addresses.rb
+++ db/migrate/20231109093007_create_addresses.rb
@@ -7,12 +7,11 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.string :phone_number
       t.string :remark
       t.boolean :is_default
-      t.reference :user_id
-      t.reference :region_id
-      t.reference :province_id
-      t.reference :city_id
-      t.reference :barangay_id
-      t.integer :role, default: 0
+      t.references :user_id
+      t.references :region_id
+      t.references :province_id
+      t.references :city_id
+      t.references :barangay_id
       t.timestamps
     end
   end
