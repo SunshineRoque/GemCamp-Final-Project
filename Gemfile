9bed3ae442fc8be509f1885a4321f784b730e0ef
SunshineRoque <sunshineararoque@gmail.com>
2023-11-10 15:48:49 +0800

    Feature 7 - Added QR code and promoter's params

--- Gemfile
+++ Gemfile
@@ -56,6 +56,7 @@ gem 'carrierwave'
 gem 'rest-client'
 gem 'active_model_serializers'
 gem 'stimulus-rails'
+gem "rqrcode", "~> 2.0"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/client/invite_controller.rb
+++ app/controllers/client/invite_controller.rb
@@ -1,8 +1,17 @@
 class Client::InviteController < ApplicationController
   include Devise::Controllers::Helpers
+  before_action :authenticate_client_user!
 
   def index
-    @user = current_client_user
-  end
+    qr_code = RQRCode::QRCode.new("client.com:3000/client/sign_up?promoter=#{current_client_user.email}") # Replace the URL with the content you want in the QR code
 
+    @svg = qr_code.as_svg(
+      color: '000',
+      shape_rendering: 'crispEdges',
+      module_size: 11,
+      standalone: true
+    )
+
+  end
 end
+
