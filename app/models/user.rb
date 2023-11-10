aa9e4bdc6db838a46035ed8bbe389db11a9e770b
SunshineRoque <sunshineararoque@gmail.com>
2023-11-10 17:46:39 +0800

    Feature 8 - Used counter_cache to increment the children_members or different way like callback, etc..

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,16 +11,22 @@ class User < ApplicationRecord
   enum role: { client: 0, admin: 1 }
 
   mount_uploader :image, ImageUploader
-  belongs_to :parent, class_name: 'User', foreign_key: 'parent_id', optional: true
+  belongs_to :parent, class_name: 'User', foreign_key: 'parent_id', optional: true, counter_cache: :children_members
   has_many :children, class_name: 'User', foreign_key: 'parent_id'
   has_many :addresses
 
   validate :validate_address_limit, on: :create
 
+  after_create :increment_parent_children_members
+
   private
 
   def validate_address_limit
     errors.add(:base, 'Exceeded maximum address limit') if addresses.count >= 5
+  end
+
+  def increment_parent_children_members
+    parent.increment!(:children_members)
   end
 
 end
