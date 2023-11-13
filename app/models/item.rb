6e289cf3a0f730e7e779e110aca4e578437213e0
SunshineRoque <sunshineararoque@gmail.com>
2023-11-13 15:19:22 +0800

    Feature 10 - Generate Item Model, Add Item model to routes and organize CRUD in controller.

---
+++ app/models/item.rb
@@ -0,0 +1,13 @@
+class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :minimum_tickets, presence: true
+  validates :state, presence: true
+  validates :batch_count, presence: true
+  validates :online_at, presence: true
+  validates :offline_at, presence: true
+  validates :start_at, presence: true
+  validates :status, presence: true
+
+end
