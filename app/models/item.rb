a41a8c851c9fd6799987443ccca3304c807f9fda
SunshineRoque <sunshineararoque@gmail.com>
2023-11-14 10:40:05 +0800

    Feature 12 - Migrated ItemCategoryShip; corrected the relationship with ItemCategoryShip and corrected the softkill CRUD of Category.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,15 @@
 class Item < ApplicationRecord
   default_scope { where(deleted_at: nil) }
-  validates :image, presence: true
   validates :name, presence: true
   validates :quantity, presence: true
   validates :minimum_tickets, presence: true
-  validates :state, presence: true
   validates :batch_count, presence: true
-  validates :online_at, presence: true
-  validates :offline_at, presence: true
-  validates :start_at, presence: true
-  validates :status, presence: true
-  validates :deleted_at, presence: true
+  enum status: { active: 0, inactive: 1 }
 
   def destroy
     update(deleted_at: Time.current)
   end
 
+  has_many :item_category_ships, dependent: :restrict_with_error
+  has_many :categories, through: :item_category_ships
 end
