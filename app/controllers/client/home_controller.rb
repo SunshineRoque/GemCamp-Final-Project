5097a32a603a7b5159fec186b8e79273f976440f
SunshineRoque <sunshineararoque@gmail.com>
2023-11-27 16:01:50 +0800

    Feature 35 - Add Winner's Feedback and Item's information in client homepage view.

--- app/controllers/client/home_controller.rb
+++ app/controllers/client/home_controller.rb
@@ -3,5 +3,7 @@ class Client::HomeController < ApplicationController
     @user = current_client_user
     @banners = Banner.where("online_at <= ? AND offline_at > ? AND status = ?", Time.current, Time.current, 'active')
     @news_tickers = NewsTicker.where("status = ?",'active').limit(5)
+    @winners = Winner.where(state: 'published').order(created_at: :desc).limit(5)
+    @items = Item.where("status = ? AND state = ?",'active', 'starting').order(created_at: :desc).limit(8)
   end
 end
